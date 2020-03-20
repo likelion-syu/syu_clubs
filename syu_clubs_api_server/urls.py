4e4602b3505b62d6cdcaa66c0274a47c26437d53
KyeongSoo Yoo <dev.yoogomja@gmail.com>
2020-03-20 14:44:56 +0900

    api 주소변경

--- syu_clubs_api_server/urls.py
+++ syu_clubs_api_server/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     # 모든 주소를 우선 client 쪽으로 연결 시킴
     url(r'^$', TemplateView.as_view(template_name='index.html'),name='index'),
     path('admin/', admin.site.urls),
-    path('api/', include('posts.urls')),
-    path('api/', include('clubs.urls'))
+    path('api/posts', include('posts.urls')),
+    path('api/clubs', include('clubs.urls'))
 ]
