bb75b75bc86ed6e58d079d4d87e7694227c2f8c4
blair06 <hyewon980610@gmail.com>
2020-03-26 14:51:29 +0900

    requirements 업데이트

--- posts/views.py
+++ posts/views.py
@@ -6,6 +6,9 @@ from rest_framework.response import Response
 from common import models
 from . import serializers
 
+from rest_framework import status
+from rest_framework import filters
+
 class PostsViewSet(APIView):
     
     def get(self, request, format=None):
@@ -14,14 +17,13 @@ class PostsViewSet(APIView):
         return Response(serializer.data)
     
 
-<<<<<<< HEAD
 class PostDetailViewSet(APIView):
     # post_id라고 해야하는지 잘 모르겠음
     def get(self, request, id, format=None):
-        queryset = Post.objects.get(id=post_id)
+        queryset = Post.objects.order_by('-date').all()
         serializer = PostSerializer(queryset, many=False)
+        filter_backends = (filters.DjangoFilterBackend,)    
+        filter_fields = ('post_id',)
         return Response(serializer.data)
-=======
     # def perform_create(self, serializer):
     #     serializer.save(user=self.request.user)
->>>>>>> 9a03f7b97cc8aa93c3f0ba322dd0aab9032081a3
