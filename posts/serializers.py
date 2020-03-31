cb7cdc08a15feb2866178ea2052eda7a68b13e74
blair06 <hyewon980610@gmail.com>
2020-03-31 14:56:32 +0900

    커밋한다

--- posts/serializers.py
+++ posts/serializers.py
@@ -6,6 +6,10 @@ class PostSerializer(serializers.ModelSerializer):
 	post_title_img_url = serializers.ReadOnlyField()
 	created_at = serializers.ReadOnlyField()
 	updated_at = serializers.ReadOnlyField()
+	user = serializers.ReadOnlyField(source='user.username')
+	is_notice = serializers.IntegerField(write_only = True)
+	is_deleted = serializers.IntegerField(write_only = True)
+	
 	# user = serializers.CharField(source='user.username')
 
 
