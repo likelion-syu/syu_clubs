10165ead241dadd23c6dfd1eb9f7817107b6be1f
joungwanpark <whddhs6645@gmail.com>
2020-04-19 02:30:49 +0900

    detail views 수정

--- clubs/views/views_detail.py
+++ clubs/views/views_detail.py
@@ -9,10 +9,3 @@ from ..serializers import serializers_detail
 class Clubs_detailViewset(viewsets.ModelViewSet):
     queryset = models.Clubs.objects.all()
     serializer_class = serializers_detail.Club_detailSerializer
-    
-    def get_queryset(self):
-        qs = super().get_queryset()
-        search = self.request.query_params.get('search','')
-        if search:
-            qs = qs.filter(club_name=search)
-        return qs   
