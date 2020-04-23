ad7d2aba2633b9b71ef31050597071c7bb4ad316
joungwanpark <whddhs6645@gmail.com>
2020-04-24 02:22:02 +0900

    relInterestclub개수 추가

--- clubs/serializers/serializers_club.py
+++ clubs/serializers/serializers_club.py
@@ -1,5 +1,9 @@
 from rest_framework import serializers
 from common import models
+class RelClubSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.RelInterestClubs
+        fields = '__all__'
 
 class ClubSerializer(serializers.ModelSerializer):
 	created_at = serializers.ReadOnlyField()
@@ -7,6 +11,10 @@ class ClubSerializer(serializers.ModelSerializer):
 	club_type = serializers.ReadOnlyField( source = 'club_type.club_type_name' )
 	club_events = serializers.ReadOnlyField ( source = 'ClubEvents.club_event_id' )
 	Posts = serializers.ReadOnlyField( source = 'Posts.post_title')
+	rel_clubs = RelClubSerializer(many=True)
+	rel_count = serializers.SerializerMethodField()
 	class Meta:
 		model = models.Clubs
 		fields = '__all__'
+	def get_rel_count(self, obj):
+		return obj.rel_clubs.count()
